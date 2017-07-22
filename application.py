e0477fc966c21f8e71cd46bf5772b027faad1e88
Tull Gearreald <budster111@gmail.com>
2017-07-22 08:59:54 -0500

    The basic framework!

--- application.py
+++ application.py
@@ -1,9 +1,7 @@
 from flask import Flask
 
-from conf import DevelopmentConfig
+from config.conf import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
-import lib.routing.rest
-import lib.routing.html
